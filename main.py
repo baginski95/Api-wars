73645bf9342ded74bbddaf883c7f0c6a7fb41d17
baginski95 <bagiczny3@gmail.com>
2020-05-06 22:24:55 +0200

    Showing table

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, escape, session, url_for
 import bcrypt
-import psycopg2
-from datetime import datetime
+import requests
 
 app = Flask(__name__)
 app.secret_key = b'_5#2211aay2L"F4Q8z\n\xec]/'
@@ -9,10 +8,14 @@ app.secret_key = b'_5#2211aay2L"F4Q8z\n\xec]/'
 
 @app.route("/")
 def start():
+    response = requests.get('https://swapi.dev/api/planets/').json()
+    next_page = response['next']
+    planets = response['results']
+    len(response)
+    print(response)
+    print(type(response))
 
-    return render_template('index.html')
-
-
+    return render_template('index.html', planets=planets, res_counter=0)
 
 
 if __name__ == "__main__":
