32d37ee58b4d055e02a99c97d333ae9773702f32
baginski95 <bagiczny3@gmail.com>
2020-05-07 01:09:36 +0200

    EventListener for button

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,7 @@
-// async function getResidents(button){
-//     let planet_url = button.dataset.planet;
-//     const result = await fetch(planet_url)
-//         .then(res => res.json());
-//     console.log(result);
-//     return result;
-//
-// }
 
 function init() {
     let resButtons = document.getElementsByClassName('res-button');
     for (let button of resButtons) {
-        console.log(button);
-
         document.body.innerHTML +=`<div class="modal" id="${button.dataset.planet_name}-residents">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -39,12 +29,14 @@ function init() {
 
         button.addEventListener('click', (e) => {
             console.log("add button");
-            // e.preventDefault();
-            // e.stopPropagation();
+             e.preventDefault();
+             e.stopPropagation();
             let planet_url = e.target.dataset.planet;
-            // let something =  getResidents(e.target);
-            // fetch(planet_url)
-            //     .then(res=> res.json())
+            fetch(planet_url)
+            .then(res=> res.json())
+                .then(resJSON => {
+                    console.log(resJSON.name);
+                })
             //     .then(data=>{
             //         console.log(data)
             //         let planetName = data.name
@@ -53,8 +45,6 @@ function init() {
             // document.body.appendChild(output);
             //     })
 
-
-            ;
             // let planetName = something.name;
             // let residents_url = something.residents;
             // console.log(planet_url);
