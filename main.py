d1f560ae32967dd327cdec6d254d592cb78770af
corumov <ppplusa@gmail.com>
2020-05-08 20:16:11 +0200

    working with next/prev buttons

--- main.py
+++ main.py
@@ -6,15 +6,38 @@ app = Flask(__name__)
 app.secret_key = b'_5#2211aay2L"F4Q8z\n\xec]/'
 
 
-@app.route("/")
-def start():
-    response = requests.get('https://swapi.dev/api/planets/').json()
-    next_page = response['next']
-    planets = response['results']
-    print(planets)
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/<button_id>", methods=['GET', 'POST'])
+def start(button_id=None):
+    if request.method != "POST":
+        # or request.form['button-prev'] == 'https://swapi.dev/api/planets/?page=1'
+        response = requests.get('https://swapi.dev/api/planets/').json()
+
+        next_page = response['next']
+        prev_page = response['previous']
+        planets = response['results']
+        print(planets)
+        print(prev_page)
+
+    else:
+        if button_id == '0':
+            print('wykonany if')
+            response = requests.get(request.form['button-prev']).json()
+            planets = response['results']
+            next_page = response['next']
+            prev_page = response['previous']
+        else:
+            print('wykonany else')
+            response = requests.get(request.form['button-next']).json()
+            planets = response['results']
+            next_page = response['next']
+            prev_page = response['previous']
 
 
-    return render_template('index.html', planets=planets, next_page=next_page)
+    return render_template('index.html', planets=planets, next_page=next_page, prev_page=prev_page)
+
+
+
 
 
 if __name__ == "__main__":
@@ -23,3 +46,4 @@ if __name__ == "__main__":
         # host='0.0.0.0',
         port=6969
     )
+
