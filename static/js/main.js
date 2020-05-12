de5c1e2252acc66abe9449dbe15737e685af6725
corumov <ppplusa@gmail.com>
2020-05-12 15:15:26 +0200

    small fixes - finishing https issue#2

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
 async function populateModalHTML(button) {
 
     let planet_url = button.dataset.planet;
-    let planet_url2 = planet_url.replace('http','https')
-    const firstResponse = await fetch(planet_url2);
+    // let planet_url2 = planet_url.replace('http','https')
+    const firstResponse = await fetch(planet_url);
     const firstJson = await firstResponse.json();
                             let output = document.createElement('div');
                             let outputContent = ''
@@ -27,9 +27,11 @@ async function populateModalHTML(button) {
                             </tr>
                         </thead>
                         <tbody>`;
-
+console.log(firstJson.residents)
         for (let resident of firstJson.residents) {
-            const resident1 = await fetch(resident);
+            let resident_with_https = await resident.replace('http','https')
+            const resident1 = await fetch(resident_with_https);
+            alert(resident_with_https)
             const residentJson = await resident1.json();
             outputContent += `<tr>
                                     <td>${residentJson.name}</td>
